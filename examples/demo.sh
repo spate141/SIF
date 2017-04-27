c17646c4f3f08b2d457dcc02b4323a9e31661550
Snehal Patel <spate141@hawk.iit.edu>
2017-04-27 11:15:18 -0500

    Update demo.sh

--- examples/demo.sh
+++ examples/demo.sh
@@ -1,12 +1,14 @@
-DATADIR=../data
-WORDFILE=$DATADIR/glove.840B.300d.txt
+DATADIR=/opt/models/vectors
+WORDFILE=$DATADIR/skip-keyword-575M-ns.vec
 
+: '
 # download word vector
 if [ ! -e $WORDFILE ]; then
 wget http://nlp.stanford.edu/data/glove.840B.300d.zip
 unzip glove.840B.300d.zip -d $DATADIR
 rm glove.840B.300d.zip
 fi
+'
 
 # demo for computing SIF embedding
 python sif_embedding.py
